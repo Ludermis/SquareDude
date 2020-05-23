44af833cc71e2a7a95771e1167806487b833da1a
Lynext <lynext12@gmail.com>
2020-05-23 07:03:48 +0300

    Lynext
    Shotgun animasyonu vs

--- Weapon2.gd
+++ Weapon2.gd
@@ -1,4 +1,4 @@
-extends Sprite
+extends AnimatedSprite
 
 var ownerNode : Node2D
 var lastShoot = -100000
@@ -41,6 +41,8 @@ func shoot (target):
 
 	
 	if !reloading && curAmmo > 0 && lastShoot + shootDelay <= Vars.time():
+		frame = 0
+		play()
 		$"../../Sounds/ShotgunBlastSound".play()
 		ownerNode.velocity.x -= (target - ownerNode.position).normalized().x * 25
 		lastShoot = Vars.time()
